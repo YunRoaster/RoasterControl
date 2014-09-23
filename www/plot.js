75540b3f6b27127d87238a31ad0788bb84d4822d
Timothy Van Heest <timothy.vanheest@gmail.com>
2014-09-23 00:18:36 -0400

    Time plot axes formatting + hover effect

--- www/plot.js
+++ www/plot.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
     // Configuration
     var palette = new Rickshaw.Color.Palette();
+    // Each is ~x50?
+    var tempScale = d3.scale.linear().domain([0, 10]).nice();
+    var powerScale = d3.scale.linear().domain([0, 2]).nice();
     var initial_data = [
-        {name: 'power'},
-        {name: 'bean'},
-        {name: 'env'}
+        {name: 'power', scale: powerScale},
+        {name: 'bean', scale: tempScale},
+        {name: 'env', scale: tempScale}
     ];
 
     time_plot = new Rickshaw.Graph({
         element: document.querySelector("#chart"),
         width: 540,
         height: 240,
-        min: 0,
-        max: 550,
         renderer: 'line',
         series: new Rickshaw.Series.FixedDuration(initial_data, palette, {
             timeInterval: 2000,
@@ -25,16 +26,33 @@ $(document).ready(function(){
         graph: time_plot
     });
 
-    var y_axis = new Rickshaw.Graph.Axis.Y({
+    var temp_axis = new Rickshaw.Graph.Axis.Y.Scaled({
         graph: time_plot,
+        grid: false,
+        pixelsPerTick: 30,
         orientation: 'left',
         tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
-        element: document.getElementById('y_axis'),
+        scale: tempScale,
+        element: document.getElementById('temp_axis'),
+    });
+
+    var power_axis = new Rickshaw.Graph.Axis.Y.Scaled({
+      graph: time_plot,
+      grid: false,
+      pixelsPerTick: 30,
+      orientation: 'right',
+      scale: powerScale,
+      tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+      element: document.getElementById('power_axis')
     });
 
     var legend = new Rickshaw.Graph.Legend({
         element: document.querySelector('#legend'),
         graph: time_plot
+    });
+
+    new Rickshaw.Graph.HoverDetail({
+      graph: time_plot
     });
 
     // The plot is ready to receive data
